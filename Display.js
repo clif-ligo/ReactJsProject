310f6412079c97b219ba2564b8ed51dca0be0a01
clif-ligo <51458206+clif-ligo@users.noreply.github.com>
2021-11-30 10:32:42 +0100

    Add files via upload

---
+++ Display.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import "./Display.css";
+
+class Display extends React.Component {
+    render(){
+        return(
+            <div className="Display">
+                {this.props.data}
+            </div>
+        );
+    }
+}
+
+export default Display;
